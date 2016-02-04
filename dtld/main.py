65ef40903dcd364cecd9a485abb6271226a07ff3
David Dexter Mwangi <dmwangimail@gmail.com>
2016-02-04 12:41:37 +0300

    Update main.py

--- dtld/main.py
+++ dtld/main.py
@@ -1,19 +1,25 @@
 __author__ = 'David Dexter'
 from DTLD.lib.protocol import Protocol
 from DTLD.lib.top_domain import TopDomain
+from DTLD.lib.startfunc import String
 class TopLevelDomain:
     def __init__(self,domain):
         self.domain = domain
     def get_top_level_domain(self):
-        check = Protocol(self.domain)
-        checkDM = check.check_protocol()
-        if checkDM == False:
-            print('Invalid Domain')
-        else:
-            get = TopDomain(self.domain)
-            srch = get.extractor()
-            if srch == False:
-                print ('Could not get domain')
+        ##check for http/https://www
+        string = String(self.domain)
+        strVar = string.formatter()
+        if strVar == False:
+            check = Protocol(self.domain)
+            checkDM = check.check_protocol()
+            if checkDM == False:
+                print('Invalid Domain')
             else:
-                #print(srch)
-                return srch
+                get = TopDomain(self.domain)
+                srch = get.extractor()
+                if srch == False:
+                    print ('Could not get domain')
+                else:
+                    return srch
+        else:
+            return strVar
